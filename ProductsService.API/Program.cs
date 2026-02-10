987ef20870822df360c9753818d732b49746e100
ashwin <ashwingholkar@outlook.com>
2026-02-10 12:51:45 +0530

    Add Swagger, CORS, and enum JSON config to API
    Enabled Swagger UI and documentation, configured CORS for localhost:4200, and set JSON serialization to handle enums as strings. Updated middleware pipeline and launch settings to open Swagger by default.

--- ProductsService.API/Program.cs
+++ ProductsService.API/Program.cs
@@ -3,6 +3,7 @@ using DataAccessLayer;
 using FluentValidation.AspNetCore;
 using ProductsService.API.APIEndpoints;
 using ProductsService.API.MIddleware;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +16,44 @@ builder.Services.AddControllers();
 
 builder.Services.AddFluentValidationAutoValidation();
 
+//add model binder to read values from json
+builder.Services.ConfigureHttpJsonOptions(opt =>
+{
+    opt.SerializerOptions.Converters.Add(new
+        JsonStringEnumConverter());
+});
+
+//add swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//add cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+//cors
+app.UseCors();
+
+//swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+//auth
+app.UseHttpsRedirection();
 app.UseAuthentication(); ;
 app.UseAuthorization();
 
